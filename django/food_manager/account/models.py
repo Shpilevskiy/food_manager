92aa9cac7b3fea47fceca570cd26d4eea86c4c80
Nick <arioxon@gmail.com>
2017-04-09 18:12:23 +0300

    Add account models.

--- django/food_manager/account/models.py
+++ django/food_manager/account/models.py
@@ -1,3 +1,25 @@
+import uuid
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+def default_expire_date():
+    return timezone.now() + timezone.timedelta(days=10)
+
+
+def generate_new_link():
+    return uuid.uuid4().hex
+
+
+class InviteLink(models.Model):
+    link = models.CharField(max_length=120, unique=True,
+                            default=generate_new_link)
+    creation_date = models.DateField(default=timezone.now)
+    expire_date = models.DateTimeField(default=default_expire_date)
+    activation_count = models.PositiveSmallIntegerField(default=3)
+
+
+class Company(models.Model):
+    title = models.CharField(max_length=120, null=False)
+    description = models.CharField(max_length=3100)
+    invite_link = models.OneToOneField(InviteLink, on_delete=models.CASCADE)
